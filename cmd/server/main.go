7385927e5305d30b3117313ae509ae797b949b6a
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2024-12-13 08:40:01 +0700

    create separate routes file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,8 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/oadultradeepfield/olympliance-server/internal/databases"
-	"github.com/oadultradeepfield/olympliance-server/internal/handlers"
-	"github.com/oadultradeepfield/olympliance-server/internal/middleware"
+	"github.com/oadultradeepfield/olympliance-server/internal/routes"
 )
 
 func main() {
@@ -21,33 +20,12 @@ func main() {
 	}
 
 	db := databases.InitDB()
-	authHandler := handlers.NewAuthHandler(db)
-	userHandler := handlers.NewUserHandler(db)
-	threadHandler := handlers.NewThreadHandler(db)
 
 	r := gin.New()
-	r.Use(middleware.CorsMiddleware())
 
-	// Health check route
-	r.GET("/health", func(c *gin.Context) {
-		c.JSON(200, gin.H{"status": "ok"})
-	})
+	routes.InitRoutes(r, db)
 
-	// Authentication routes
-	r.POST("/api/register", authHandler.Register)
-	r.POST("/api/login", authHandler.Login)
-	r.GET("/api/threads/:id", threadHandler.GetThread)
-	r.GET("/api/threads/category/:category_id", threadHandler.GetAllThreadsByCategory)
-
-	// Protected routes
-	api := r.Group("/api")
-	api.Use(middleware.AuthMiddleware(db))
-
-	api.POST("/change-password", userHandler.ChangePassword)
-
-	api.POST("/threads", threadHandler.CreateThread)
-	api.PUT("/threads/:id", threadHandler.UpdateThread)
-	api.DELETE("/threads/:id", threadHandler.DeleteThread)
-
-	r.Run(":" + os.Getenv("PORT"))
+	if err := r.Run(":" + os.Getenv("PORT")); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
---
+++ internal/routes/route.go
@@ -0,0 +1,47 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/oadultradeepfield/olympliance-server/internal/handlers"
+	"github.com/oadultradeepfield/olympliance-server/internal/middleware"
+	"gorm.io/gorm"
+)
+
+func InitRoutes(r *gin.Engine, db *gorm.DB) {
+	authHandler := handlers.NewAuthHandler(db)
+	userHandler := handlers.NewUserHandler(db)
+	threadHandler := handlers.NewThreadHandler(db)
+	commentHandler := handlers.NewCommentHandler(db)
+
+	r.Use(middleware.CorsMiddleware())
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+
+	// Unprotected Routes
+	r.GET("/api/threads/:id", threadHandler.GetThread)
+	r.GET("/api/threads/category/:category_id", threadHandler.GetAllThreadsByCategory)
+	r.GET("/api/comments/:id", commentHandler.GetComment)
+	r.GET("/api/comments", commentHandler.GetAllComments)
+
+	// Authentication Routes
+	r.POST("/api/register", authHandler.Register)
+	r.POST("/api/login", authHandler.Login)
+
+	// Protected Routes
+	api := r.Group("/api")
+	api.Use(middleware.AuthMiddleware(db))
+
+	api.POST("/change-password", userHandler.ChangePassword)
+
+	// Threads
+	api.POST("/threads", threadHandler.CreateThread)
+	api.PUT("/threads/:id", threadHandler.UpdateThread)
+	api.DELETE("/threads/:id", threadHandler.DeleteThread)
+
+	// Comments
+	api.POST("/comments", commentHandler.CreateComment)
+	api.PUT("/comments/:id", commentHandler.UpdateComment)
+	api.DELETE("/comments/:id", commentHandler.DeleteComment)
+}
