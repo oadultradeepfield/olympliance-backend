b90ac7684549d8a6a91c7ad7cfe75b7bf6aa7941
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2024-12-14 08:15:04 +0700

    update file names

---
+++ internal/databases/databases.go
@@ -0,0 +1,62 @@
+package databases
+
+import (
+	"log"
+	"os"
+
+	"github.com/oadultradeepfield/olympliance-server/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func InitDB() *gorm.DB {
+	dsn := os.Getenv("DSN")
+	if dsn == "" {
+		log.Fatalf("DSN is not set in the environment variables")
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger:      logger.Default.LogMode(logger.Info),
+		PrepareStmt: true,
+	})
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(
+		&models.User{},
+		&models.Thread{},
+		&models.Comment{},
+		&models.Interaction{},
+		&models.Category{},
+	); err != nil {
+		log.Fatalf("Error migrating database: %v", err)
+	}
+
+	categories := []models.Category{
+		{Name: "General"},
+		{Name: "Mathematics"},
+		{Name: "Physics"},
+		{Name: "Chemistry"},
+		{Name: "Informatics"},
+		{Name: "Biology"},
+		{Name: "Philosophy"},
+		{Name: "Astronomy"},
+		{Name: "Geography"},
+		{Name: "Linguistics"},
+		{Name: "Earth Science"},
+	}
+
+	for _, category := range categories {
+		result := db.FirstOrCreate(&category, models.Category{Name: category.Name})
+		if result.Error != nil {
+			log.Fatalf("Error creating category %s: %v\n", category.Name, result.Error)
+		} else {
+			log.Printf("Category '%s' checked/added successfully!\n", category.Name)
+		}
+	}
+
+	log.Println("Database connected, migrated, and categories added successfully")
+	return db
+}
---
+++ internal/routes/routes.go
@@ -0,0 +1,55 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/oadultradeepfield/olympliance-server/internal/handlers"
+	"github.com/oadultradeepfield/olympliance-server/internal/middleware"
+	"gorm.io/gorm"
+)
+
+func InitRoutes(r *gin.Engine, db *gorm.DB) {
+	authHandler := handlers.NewAuthHandler(db)
+	userHandler := handlers.NewUserHandler(db)
+	threadHandler := handlers.NewThreadHandler(db)
+	commentHandler := handlers.NewCommentHandler(db)
+	interactionHandler := handlers.NewInteractionHandler(db)
+
+	r.Use(middleware.CorsMiddleware())
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+
+	// Unprotected Routes
+	r.GET("/api/threads/:id", threadHandler.GetThread)
+	r.GET("/api/threads/category/:category_id", threadHandler.GetAllThreadsByCategory)
+	r.GET("/api/comments", commentHandler.GetAllComments)
+
+	// Authentication Routes
+	r.POST("/api/register", authHandler.Register)
+	r.POST("/api/login", authHandler.Login)
+
+	// Protected Routes
+	api := r.Group("/api")
+	api.Use(middleware.AuthMiddleware(db))
+	api.Use(middleware.BanCheckMiddleware(db))
+
+	// Users
+	api.PUT("/users/change-password", userHandler.ChangePassword)
+	api.PUT("/users/:id/toggle-ban", userHandler.ToggleBanUser)
+	api.PUT("/users/:id/toggle-moderator", userHandler.ToggleAssignModerator)
+
+	// Threads
+	api.POST("/threads", threadHandler.CreateThread)
+	api.PUT("/threads/:id", threadHandler.UpdateThread)
+	api.DELETE("/threads/:id", threadHandler.DeleteThread)
+
+	// Comments
+	api.POST("/comments", commentHandler.CreateComment)
+	api.PUT("/comments/:id", commentHandler.UpdateComment)
+	api.DELETE("/comments/:id", commentHandler.DeleteComment)
+
+	// Interaction
+	api.POST("/interactions", interactionHandler.CreateInteraction)
+	api.PUT("/interactions/:id", interactionHandler.UpdateInteraction)
+}
